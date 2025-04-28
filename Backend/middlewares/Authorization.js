82f8d0cd466afc39a2511b13dda95ca204632baa
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-04-28 14:58:45 +0300

    #######

---
+++ Backend/middlewares/Authorization.js
@@ -0,0 +1,9 @@
+const allowedUsers = (...allowedRoles) => {
+    return (req, res, next) => {
+      if (!req.user || !allowedRoles.includes(req.user.role)) {
+        return res.status(403).json({ message: "Access Denied" });
+      }
+      next();
+    };
+  };
+  export default allowedUsers;
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { Register } from "../controllers/authControllers.js";
+import { Approval, Login, Logout, Register } from "../controllers/authControllers.js";
+
 const router = express.Router()
 router.post('/register', Register)
+router.post('/login', Login)
+router.post('/logout', Logout)
+router.post('/approval/:id', Approval)
 export default router
