4ea221fd39ad4dcef677f8ee36bf5e18e5b8da01
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-05-08 16:59:53 +0300

    socket

---
+++ Backend/Socket/socket.js
@@ -0,0 +1,18 @@
+import { Server } from 'socket.io';
+import express from "express";
+import http from 'http';
+
+const app = express();
+const server = http.createServer(app); 
+const io = new Server(server, {
+    cors: {
+        origin: ['http://localhost:5173', 'http://localhost:8080', 'https://preview--role-based-education-sphere.lovable.app'],
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("user connected", socket.id);
+});
+
+export { io, server, app };
