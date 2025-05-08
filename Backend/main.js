a5b6ab12fb22fc17ff670bae6ce9b6197cb65ea1
fayinana <fekedeananiya@gmail.com>
2025-05-08 11:25:01 +0300

    add a new route to get all posts

--- Backend/main.js
+++ Backend/main.js
@@ -1,41 +1,47 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import Database from './database/database.js'
-import cookieParser from 'cookie-parser'
-import authRoutes from "./routes/authRoutes.js"
-import messagesRoutes from './routes/messagesRoutes.js'
-import resource from './routes/resourceRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import instructionalRoutes from './routes/instructional.js'
-import engagementRoutes from "./routes/engagementRoutes.js"
-import postRoutes from './routes/postRoutes.js'
-import cors from "cors"
-const app=express()
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
+import express from "express";
+import dotenv from "dotenv";
+import Database from "./database/database.js";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js";
+import messagesRoutes from "./routes/messagesRoutes.js";
+import resource from "./routes/resourceRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import instructionalRoutes from "./routes/instructional.js";
+import engagementRoutes from "./routes/engagementRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import cors from "cors";
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 //initializing cors
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:8080'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:8080",
+      "https://preview--role-based-education-sphere.lovable.app/",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 dotenv.config();
-const port=process.env.PORT
+const port = process.env.PORT;
 //initializing database
-Database()
+Database();
 //initializing routes
-app.use('/api/auth', authRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/messages', messagesRoutes)
-app.use('/api/resources', resource)
-app.use('/api/instructional', instructionalRoutes)
-app.use('/api/engagement', engagementRoutes)
-app.use('/api/post', postRoutes)
-app.get('/', (req, res) => {
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/messages", messagesRoutes);
+app.use("/api/resources", resource);
+app.use("/api/instructional", instructionalRoutes);
+app.use("/api/engagement", engagementRoutes);
+app.use("/api/post", postRoutes);
+app.get("/", (req, res) => {
   res.send("Server is running 🚀");
 });
-app.listen(port,()=>{
-    console.log(`server is running 🚀 on port http://localhost:/${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is running 🚀 on port http://localhost:/${port}`);
+});
